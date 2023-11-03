404b4ba4d4c5b87e36397a1a8d1f082524a8a2fc
Vitor George <vitor.george@gmail.com>
2023-11-03 16:47:37 +0000

    Create apps/ folder, fix prisma schema issue

---
+++ apps/cli/postinstall.js
@@ -0,0 +1,24 @@
+import fs from "fs-extra";
+
+// Prisma doesn't handle well when the schema file is not present in the same
+// sub-tree of package.json. So we copy the schema file from apps/next/prisma
+// to apps/cli/prisma after the installation.
+
+/**
+ * Prisma doesn't handle well when the schema file is not present in the same
+ *  sub-tree of package.json. So we copy the schema file from apps/next/prisma
+ * to apps/cli/prisma after the installation.
+ * See
+ *
+ * - https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
+ * - https://github.com/vercel/next.js/issues/46070
+ * - https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+ */
+
+(async () => {
+  // remove prisma folder
+  await fs.remove("./prisma");
+
+  // copy prisma folder from apps/next/prisma
+  await fs.copy("../next/prisma", "./prisma");
+})();
