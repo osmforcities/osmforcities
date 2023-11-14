508ff7640f0e534e94c33f5f58052fabf9a0abbd
Vitor George <vitor.george@gmail.com>
2023-11-14 11:11:11 +0000

    Add maintenance mode env var

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,27 @@ import React from "react";
 import { SearchInput } from "./components/search";
 
 const HomePage = () => {
+  if (process.env.MAINTENANCE_MODE === "true") {
+    return (
+      <div role="main" className="flex justify-center items-center h-screen">
+        <div className="text-center">
+          <h1 className="text-xl font-bold mb-4">Under Maintenance</h1>
+          <p className="pb-3">
+            We are currently updating this page. Please check back later.
+          </p>
+          <a
+            href="https://github.com/osmforcities/osmforcities"
+            className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit our GitHub Repository
+          </a>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div role="main">
       <SearchInput />
