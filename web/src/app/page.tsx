617ff3a229b4d6dcb8fe74c774311a6d8ad2d1c6
Vitor George <vitor.george@gmail.com>
2023-11-15 18:49:45 +0000

    Allow setting maintenance message as environment variable

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { SearchInput } from "../components/search";
 
+const maintenanceMessage =
+  process.env.MAINTENANCE_MESSAGE ||
+  "OSM for Cities is currently under maintenance. Please check back later.";
+
 const HomePage = () => {
   if (process.env.MAINTENANCE_MODE === "true") {
     return (
       <div role="main" className="flex justify-center items-center h-screen">
         <div className="text-center">
           <h1 className="text-xl font-bold mb-4">Under Maintenance</h1>
-          <p className="pb-3">
-            We are currently updating this page. Please check back later.
-          </p>
+          <p className="pb-3">{maintenanceMessage}</p>
           <a
             href="https://github.com/osmforcities/osmforcities"
             className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
