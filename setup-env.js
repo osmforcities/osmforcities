a9815f52e1d1fa0cf7744c5bc70f172963b1a1d0
Vitor George <vitor.george@gmail.com>
2023-12-02 18:42:33 +0000

    Reset .env symlink on every install.
    The symlinks will not point to the correct path if the repository folder is moved. With this change, the symlinks can be reset easily by running `pnpm install` or running the script `setup-env.js` directly.

--- setup-env.js
+++ setup-env.js
@@ -12,22 +12,25 @@ if (!fs.existsSync(envPath)) {
   );
 }
 
-// Create symlinks to .env file in all apps
-const apps = ["cli", "web"];
-apps.forEach((app) => {
-  const appPath = path.join(__dirname, `./apps/${app}/.env`);
+function removeSymlinkIfItExists(filePath) {
   try {
-    const stats = fs.lstatSync(appPath);
-    if (!stats.isSymbolicLink()) {
-      console.log(`Creating .env symlink for ${app} app.`);
-      fs.symlinkSync(envPath, appPath);
+    const stats = fs.lstatSync(filePath);
+    if (stats.isSymbolicLink()) {
+      console.log(`Removing existing .env symlink at ${filePath}`);
+      fs.unlinkSync(filePath);
     }
   } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(`Creating .env symlink for ${app} app.`);
-      fs.symlinkSync(envPath, appPath);
-    } else {
+    if (error.code !== "ENOENT") {
       throw error; // Rethrow if the error is not related to the absence of the file
     }
   }
+}
+
+// Create symlinks to .env file in all apps
+const apps = ["cli", "web"];
+apps.forEach((app) => {
+  const appEnvPath = path.join(__dirname, `./apps/${app}/.env`);
+  removeSymlinkIfItExists(appEnvPath);
+  console.log(`Creating .env symlink for ${app} app.`);
+  fs.symlinkSync(envPath, appEnvPath);
 });
