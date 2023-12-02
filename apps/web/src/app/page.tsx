ea7c6b360509a8e62ba40c5928087b3390b2dc5f
Vitor George <vitor.george@gmail.com>
2023-12-02 21:11:03 +0000

    Improve the home page. Changes copy, add sections, display latest updates.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,32 +1,111 @@
 import React from "react";
 import { SearchInput } from "./components/search";
+import { fetchLatestChanges } from "./fetch";
+import { formatDistanceToNow } from "date-fns";
+import { Link } from "./components/common";
+
+const LatestChangesSection = async () => {
+  const latestChanges = await fetchLatestChanges();
+
+  if (!latestChanges) {
+    return null;
+  }
+
+  return (
+    <section id="latest-changes">
+      <div className="flex flex-col mx-auto mt-10">
+        <h2 className="text-left text-4xl mb-10">Latest changes</h2>
+        <table className="table-auto">
+          <thead className="sr-only">
+            <tr>
+              <th>Preset</th>
+              <th>City</th>
+              <th>Updated At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {latestChanges.map(
+              ({ presetName, presetUrl, cityName, cityUrl, updatedAt }) => {
+                return (
+                  <tr key={presetUrl} className="border-b border-gray-300">
+                    <td className="font-bold py-2 px-2">
+                      <Link href={presetUrl}>{presetName}</Link>
+                    </td>
+                    <td className="py-2 px-2">
+                      <Link href={cityUrl}>{cityName}</Link>
+                    </td>
+                    <td className="font-thin py-2 pl-2 pr-5 text-right">
+                      {formatDistanceToNow(updatedAt, {
+                        addSuffix: true,
+                      })}
+                    </td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+const SearchSection = () => {
+  return (
+    <section id="search">
+      <div className="flex flex-col mx-auto mt-10">
+        <h2 className="text-left text-4xl mb-10">Find a city</h2>
+        <div className="w-full flex">
+          <SearchInput />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="flex flex-col mx-auto mt-10">
+        <h2 className="text-left text-4xl mb-10">About</h2>
+        <div className=" text-lg pb-10">
+          This platform is in early development and the coverage it restricted
+          to Brazil.{" "}
+          <a href="/about" className="text-blue-500 hover:text-blue-700">
+            Please visit the about page.
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const HeroSection = () => {
+  return (
+    <section id="hero">
+      <div className="flex flex-col mx-auto">
+        <h1 className="text-center text-6xl mt-20 mb-10 font-bold">
+          <span className="block">Discover free maps</span>
+          <span className="block">of your city.</span>
+        </h1>
+        <div className="text-center text-2xl pb-5 font-light">
+          OSM for Cities is a platform that provides daily updated city-level
+          maps from OpenStreetMap, the map of the world where everyone can
+          collaborate.
+        </div>
+      </div>
+    </section>
+  );
+};
 
 const HomePage = () => {
   return (
-    <div role="main" aria-label="home" className="p-4">
-      <div className="flex flex-col mx-auto max-w-lg">
-        <h1 className="text-center text-6xl mt-20 mb-10">
-          Open maps of your city.
-        </h1>
-        <div className="text-center text-2xl pb-10">
-          Download, inspect and track data from OpenStreetMap, the open map for
-          the world.
-        </div>
-        <div className="text-center text-2xl pb-5">
-          Start by finding your city:
-        </div>
-        <div className="w-full flex justify-center pb-10">
-          <SearchInput />
-        </div>
-        <div className="text-center text-xl pb-5">
-          This platform is in early development and the coverage it restricted
-          to Brazil.
-        </div>
-        <div className="text-center text-lg pb-5">
-          <a href="/about" className="text-blue-500 hover:text-blue-700">
-            Please visit the about page to learn more.
-          </a>
-        </div>
+    <div role="main" aria-label="home">
+      <div className="flex flex-col mx-auto max-w-2xl">
+        <HeroSection />
+        <LatestChangesSection />
+        <SearchSection />
+        <AboutSection />
       </div>
     </div>
   );
