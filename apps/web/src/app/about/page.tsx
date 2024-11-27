a20a208804c73c2357a77c770c1e2980a6cd3fa5
Vitor George <vitor.george@gmail.com>
2024-11-27 19:57:21 +0000

    Update about page

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -10,50 +10,50 @@ import PageLayout from "../components/page-layout";
 const AboutPage = async () => {
   return (
     <PageLayout aria-label="about">
-      <PageTitle>Daily updated datasets from OpenStreetMap</PageTitle>
+      <PageTitle>Daily Updated Datasets from OpenStreetMap</PageTitle>
       <Paragraph>
-        OSM for Cities is a platform to provide easy access to city-level
-        OpenStreetMap data. It is aimed to simplify retrieving and utilizing OSM
-        data, enabling cities to make informed decisions and leveraged by the
-        power of local mapping.
+        OSM for Cities is a platform designed to provide easy access to
+        city-level OpenStreetMap data. It aims to simplify retrieving and
+        utilizing OSM data, enabling cities to make informed decisions while
+        leveraging the power of local mapping.
       </Paragraph>
       <Paragraph>
         We have developed a tool that tracks daily-updated city-level OSM data
-        in a git repository. This service processes daily diffs of predefined
-        OSM presets, such as schools, hospitals, cycling infrastructure, etc.
-        The processed data is then pushed to the git repository, making it
-        readily available for direct access or integration into third-party
+        in a Git repository. This service processes daily diffs of predefined
+        OSM presets, such as schools, hospitals, cycling infrastructure, and
+        more. The processed data is then pushed to the Git repository, making it
+        readily available for direct access or integration with third-party
         applications.
       </Paragraph>
-      <PageSubtitle>Key features</PageSubtitle>
+      <PageSubtitle>Key Features</PageSubtitle>
       <Paragraph>
-        This platform ensures timeliness by updating data daily, reflecting the
-        latest changes in OpenStreetMap. Additionally, a git server enables
-        users how files have evolved over time.
+        This platform ensures timeliness by updating data daily to reflect the
+        latest changes in OpenStreetMap. Additionally, a Git server allows users
+        to see how files have evolved over time.
       </Paragraph>
       <Paragraph>
-        Coverage is another crucial aspect of OSM for Cities. Each city within
-        in the platform can potentially offer 30+ datasets, allowing for a
-        comprehensive understanding of various urban aspects. We aim to include
-        data from all countries on our platform, making city-level OSM data
-        accessible globally.
+        Coverage is another crucial aspect of OSM for Cities. Each city on the
+        platform can potentially offer over 30 datasets, providing a
+        comprehensive understanding of various urban aspects. Our goal is to
+        include data from all countries on our platform, making city-level OSM
+        data accessible globally.
       </Paragraph>
       <Paragraph>
-        In the first prototype of this platform, the coverage is restricted to
+        In its first prototype, the platform&apos;s coverage is restricted to
         Brazil. With more than five thousand municipalities and ranking among
-        the top 10 countries in number of OSM elements, Brazil showcases the
+        the top 10 countries in the number of OSM elements, Brazil showcases the
         potential of the platform. As we expand, the next challenge is to add
         more countries to OSM for Cities.
       </Paragraph>
-      <PageSubtitle>The data processing workflow</PageSubtitle>
+      <PageSubtitle>The Data Processing Workflow</PageSubtitle>
       <Paragraph>
         The process begins by recursively slicing the Planet PBF file to the
         city level. City-level presets are then extracted and converted into
-        GeoJSON format. The datasets are available in GeoJSON format, enclosed
-        within the city limits polygon, reducing the need for pre-processing.
-        This ready-to-use approach saves time for anyone interested in using the
-        data and make it easier to integrate of OpenStreetMap data into
-        workflows.
+        GeoJSON format. These datasets are available in GeoJSON format and are
+        enclosed within city limit polygons, reducing the need for
+        pre-processing. This ready-to-use approach saves time for anyone
+        interested in using the data and makes it easier to integrate
+        OpenStreetMap data into workflows.
       </Paragraph>
       <PageSubtitle>Contact & Feedback</PageSubtitle>
       <Paragraph>
@@ -64,7 +64,7 @@ const AboutPage = async () => {
       </Paragraph>
       <PageSubtitle>About</PageSubtitle>
       <Paragraph>
-        This project is part of the Labs program of{" "}
+        This project is part of the Labs program by{" "}
         <ExternalLink href="https://developmentseed.org">
           Development Seed
         </ExternalLink>
@@ -73,12 +73,12 @@ const AboutPage = async () => {
       <Paragraph>
         The source code for this project is open and{" "}
         <ExternalLink href="https://github.com/osmforcities/osmforcities">
-          available at Github
+          available on GitHub
         </ExternalLink>
         .
       </Paragraph>
       <Paragraph>
-        OSM for Cities was idealized by{" "}
+        OSM for Cities was conceptualized by{" "}
         <ExternalLink href="https://github.com/vgeorge">
           Vitor George
         </ExternalLink>
