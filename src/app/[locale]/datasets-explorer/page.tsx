fb662cbf075baafe391ea9142396a342c77f1cc0
Vitor George <vitor.george@gmail.com>
2025-09-03 19:11:13 +0100

    feat: add datasets explorer page with v0-inspired layout

---
+++ src/app/[locale]/datasets-explorer/page.tsx
@@ -0,0 +1,11 @@
+export default function DatasetsV2Page() {
+  const title = "Datasets V2";
+  const description = "Simple test page";
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
